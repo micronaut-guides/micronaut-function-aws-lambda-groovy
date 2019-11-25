9932df9e1d92f56ba32a7e2f1c7794d4ff7a9702
Iván López <lopezi@objectcomputing.com>
2019-11-25 13:35:20 +0100

    Minor cleanup in guide

--- complete/invoice/src/main/groovy/example/micronaut/InvoiceController.groovy
+++ complete/invoice/src/main/groovy/example/micronaut/InvoiceController.groovy
@@ -5,14 +5,12 @@ import io.micronaut.context.annotation.Value
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
-import io.micronaut.validation.Validated
 import io.reactivex.Single
 
 import javax.validation.Valid
 
 @CompileStatic
-@Validated // <1>
-@Controller("/invoice") // <2>
+@Controller("/invoice") // <1>
 class InvoiceController {
 
     private final BigDecimal vatPercentage
@@ -21,17 +19,17 @@ class InvoiceController {
 
     private final int scale
 
-    InvoiceController(@Value('${vat.percentage}') BigDecimal vatPercentage, // <3>
+    InvoiceController(@Value('${vat.percentage}') BigDecimal vatPercentage, // <2>
                       @Value('${vat.scale}') int scale,
-                      VatValidator vatValidator) { // <4>
+                      VatValidator vatValidator) { // <3>
         this.vatPercentage = vatPercentage
         this.scale = scale
         this.vatValidator = vatValidator
     }
 
-    @Post("/vat") // <5>
-    Single<Taxes> calculateVat(@Valid @Body Invoice invoice) {  // <6>
-        vatValidator.validateVat(new VatValidationRequest(memberStateCode: invoice.countryCode, vatNumber: invoice.vatNumber)) // <7>
+    @Post("/vat") // <4>
+    Single<Taxes> calculateVat(@Valid @Body Invoice invoice) {  // <5>
+        vatValidator.validateVat(new VatValidationRequest(memberStateCode: invoice.countryCode, vatNumber: invoice.vatNumber)) // <6>
                 .map { VatValidation vatValidation ->
                     BigDecimal percentage = vatValidation.valid ? vatPercentage : new BigDecimal("0")
                     new Taxes(vat: invoice.lines.stream()
